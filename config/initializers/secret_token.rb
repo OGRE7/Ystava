c3c8603ba4dbc084c99af6fb2f9e4478984d606b
OGRE7 <paulintrognon@gmail.com>
2012-10-23 20:09:05 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ystava::Application.config.secret_token = '8c78dbc5ee23d5e3de51617a5aa1f90a1eafe52f1f147866f1048647fb47378a29ab7bb87befd4fbd0926afc83b8a2f9d9f1a72b2b0d360012a36fba93e60f10'
