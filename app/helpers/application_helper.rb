3af52426ab043152d86c19ad72f95a18f4b85778
OGRE7 <paulintrognon@gmail.com>
2012-10-24 15:26:16 -0400

    ajout helper pour titre page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.       # Documentation comment
+  def titre_complet(titre_page)                          # Method definition
+    titre_base = "Ruby on Rails Tutorial Sample App"  # Variable assignment
+    if titre_page.empty?                              # Boolean test
+      titre_base                                      # Implicit return
+    else
+      "#{titre_page} - #{titre_base}"                 # String interpolation
+    end
+  end
 end
