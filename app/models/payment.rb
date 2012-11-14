0880f91fb755ffc9c02e8f84fc95c8225bd71a51
OGRE7 <paulintrognon@gmail.com>
2012-11-14 01:53:08 -0500

    finalisation branche

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,11 @@
+class NotTheSameValidator < ActiveModel::Validator
+  def validate(record)
+    if record.utilisateur_id == record.vers_utilisateur_id
+      record.errors[:base] << "Les deux personnes doivent-etre differentes !"
+    end
+  end
+end
+
 class Payment < ActiveRecord::Base
   attr_accessible :description, :somme, :utilisateur_id, :vers_utilisateur_id
   
@@ -7,9 +15,11 @@ class Payment < ActiveRecord::Base
   validates :somme, presence: true
   validates :utilisateur_id, presence: true
   validates :vers_utilisateur_id, presence: true
+  validates_with NotTheSameValidator
   
   def vers
     Utilisateur.find(vers_utilisateur_id)
   end
   
 end
+
